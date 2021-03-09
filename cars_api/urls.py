8d1e4e0192e949372a2a6967e8769d69c0216822
ibinkows <igor.binkowski@nokia.com>
2021-03-09 19:24:43 +0100

    added view names in cars_api

--- cars_api/urls.py
+++ cars_api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import car_list_and_create_view, car_delete_view, rate_car_view, popular_car_list_view
+from .views import car_list_and_create_view, car_delete_view, car_rate_view, popular_car_list_view
 
 urlpatterns = [
-    path('cars/', car_list_and_create_view),
-    path('cars/<int:car_id>/', car_delete_view),
-    path('rate/', rate_car_view),
-    path('popular/', popular_car_list_view),
+    path('cars/', car_list_and_create_view, name='car-list-and-create'),
+    path('cars/<int:car_id>/', car_delete_view, name='car-delete'),
+    path('rate/', car_rate_view, name='car-rate'),
+    path('popular/', popular_car_list_view, name='popular-car-list'),
     ]
